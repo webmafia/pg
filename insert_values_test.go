6e3012e64464b6c58d7fe08b8004ff73502758b7
Ivar Johansson <ivar@webbmaffian.se>
2024-03-13 18:15:28 +0100

    Clean-up

--- insert_values_test.go
+++ insert_values_test.go
@@ -5,23 +5,6 @@ import (
 	"fmt"
 )
 
-// func BenchmarkInsert(b *testing.B) {
-// 	db := NewDB(nil)
-
-// 	b.ResetTimer()
-
-// 	for i := 0; i < b.N; i++ {
-// 		db.Insert(context.Background(), Identifier("mjau"),
-// 			"foo", "bar",
-// 			"foo", "bar",
-// 			"foo", "bar",
-// 			"foo", "bar",
-// 			"foo", "bar",
-// 			"foo", "bar",
-// 		)
-// 	}
-// }
-
 func ExampleInsert() {
 	db := NewDB(nil)
 
