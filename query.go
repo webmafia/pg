3458e5f4ab6488133fad94b6f67316a1a42c9c17
Ivar Johansson <ivar@webbmaffian.se>
2024-03-13 10:28:20 +0100

    WIP

--- query.go
+++ query.go
@@ -2,61 +2,9 @@ package pg
 
 import (
 	"strings"
-	"unsafe"
 
 	"github.com/webmafia/fast"
 )
-
-var _ QueryEncoder = Query("")
-
-type Query string
-
-// EncodeQuery implements QueryEncoder.
-func (q Query) EncodeQuery(buf *fast.StringBuffer, args []any, queryArgs *[]any) {
-	var cursor int
-	argNum := 1
-	format := *(*string)(unsafe.Pointer(&q))
-
-	for {
-		if len(args) == 0 {
-			break
-		}
-
-		i := strings.IndexByte(format[cursor:], '%')
-
-		if i < 0 {
-			break
-		}
-
-		idx := cursor + i
-		i = idx + 1
-		buf.WriteString(format[cursor:idx])
-		cursor = idx + 2
-
-		// Double % means an escaped %
-		if format[i] == '%' {
-			buf.WriteByte('%')
-			continue
-		}
-
-		c := format[i]
-
-		if ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') {
-			if enc, ok := args[0].(fast.StringEncoder); ok {
-				enc.EncodeString(buf)
-			} else {
-				buf.WriteByte('$')
-				buf.WriteInt(argNum)
-				*queryArgs = append(*queryArgs, args[0])
-				argNum++
-			}
-
-			args = args[1:]
-		}
-	}
-
-	buf.WriteString(format[cursor:])
-}
 
 func encodeQuery(buf *fast.StringBuffer, format string, args []any, queryArgs *[]any) {
 	var cursor int
--- query_test.go
+++ query_test.go
@@ -10,8 +10,8 @@ import (
 func Example_encodeQuery() {
 	buf := fast.NewStringBuffer(256)
 	var queryArgs []any
-	q := Query("SELECT * FROM %T WHERE foo = %d AND bar = %s AND baz = %s")
-	q.EncodeQuery(buf, []any{Table("trudeluttan"), 123, Table("mjau"), 456}, &queryArgs)
+	// q := Query("SELECT * FROM %T WHERE foo = %d AND bar = %s AND baz = %s")
+	// q.EncodeQuery(buf, []any{Table("trudeluttan"), 123, Table("mjau"), 456}, &queryArgs)
 	// encodeQuery(buf, "SELECT * FROM %T WHERE foo = %d AND bar = %s AND baz = %s", []any{Table("trudeluttan"), 123, Table("mjau"), 456}, &queryArgs)
 
 	fmt.Println(buf.String(), queryArgs)
@@ -27,20 +27,6 @@ func Benchmark_encodeQuery(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		encodeQuery(buf, "SELECT * FROM %T WHERE foo = %d", []any{Table("trudeluttan"), 123}, &queryArgs)
-		buf.Reset()
-		queryArgs = queryArgs[:0]
-	}
-}
-
-func Benchmark_encodeQuery2(b *testing.B) {
-	buf := fast.NewStringBuffer(256)
-	queryArgs := make([]any, 0, 5)
-
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		q := Query("SELECT * FROM %T WHERE foo = %d AND bar = %s AND baz = %s")
-		q.EncodeQuery(buf, []any{Table("trudeluttan"), 123, Table("mjau"), 456}, &queryArgs)
 		buf.Reset()
 		queryArgs = queryArgs[:0]
 	}
