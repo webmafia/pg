a572509c7493a2baf804b210ecd2332b43d8c859
Ivar Johansson <ivar@webbmaffian.se>
2024-03-13 09:55:07 +0100

    WIP

--- query.go
+++ query.go
@@ -1,163 +1,53 @@
 package pg
 
 import (
-	"fmt"
+	"strings"
 
 	"github.com/webmafia/fast"
 )
 
-// func (db *DB) formatQuery(sql string, args ...any) {
-// 	q := `
-// 		SELECT *
-// 		FROM %T
-// 		WHERE %T
+func encodeQuery(buf *fast.StringBuffer, format string, args []any) {
+	var cursor, argIdx int
+	argNum := 1
 
-// 	`
-// }
-
-func indexQuery(format string) (q indexedQuery) {
-	end := len(format)
-
-formatLoop:
-	for i := 0; i < end; {
-		for i < end && format[i] != '%' {
-			i++
-		}
-
-		if i >= end {
-			// done processing format string
+	for {
+		if argIdx >= len(args) {
 			break
 		}
 
-		idx := i
+		i := strings.IndexByte(format[cursor:], '%')
 
-		// Process one character (after %)
-		i++
+		if i < 0 {
+			break
+		}
 
-		// Double % means escape
+		idx := cursor + i
+		i = idx + 1
+		buf.WriteString(format[cursor:idx])
+		cursor = idx + 2
+
+		// Double % means an escaped %
 		if format[i] == '%' {
-			i++
-			continue formatLoop
-		}
-
-		if i >= end {
-			break
+			buf.WriteByte('%')
+			continue
 		}
 
 		c := format[i]
 
 		if ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') {
-			i++
-			q.indices = append(q.indices, idx)
+			arg := args[argIdx]
 
-			continue formatLoop
-		}
-	}
-
-	q.query = format
-
-	return
-}
-
-type indexedQuery struct {
-	query   string
-	indices []int
-}
-
-func (q indexedQuery) encodeQuery(buf *fast.StringBuffer, args []any) (err error) {
-	if expect, got := len(q.indices), len(args); expect != got {
-		return fmt.Errorf("invalid number of arguments; expected %d, got %d", expect, got)
-	}
-
-	// Short circuit if there are no args
-	if len(q.indices) == 0 {
-		buf.WriteString(q.query)
-		return
-	}
-
-	var last int
-	argNum := 1
-
-	for i, idx := range q.indices {
-		buf.WriteString(q.query[last:idx])
-		last = idx + 2
-
-		if enc, ok := args[i].(fast.StringEncoder); ok {
-			enc.EncodeString(buf)
-		} else {
-			buf.WriteByte('$')
-			buf.WriteInt(argNum)
-			argNum++
-		}
-	}
-
-	buf.WriteString(q.query[last:])
-
-	return
-}
-
-// func
-
-// argNumber returns the next argument to evaluate, which is either the value of the passed-in
-// argNum or the value of the bracketed integer that begins format[i:]. It also returns
-// the new value of i, that is, the index of the next byte of the format to process.
-func argNumber(argNum int, format string, i int) (newArgNum, newi int, found bool) {
-	if len(format) <= i || format[i] != '[' {
-		return argNum, i, false
-	}
-
-	index, wid, ok := parseArgNumber(format[i:])
-
-	if ok && 0 <= index {
-		return index, i + wid, true
-	}
-
-	return argNum, i + wid, ok
-}
-
-// parseArgNumber returns the value of the bracketed number, minus 1
-// (explicit argument numbers are one-indexed but we want zero-indexed).
-// The opening bracket is known to be present at format[0].
-// The returned values are the index, the number of bytes to consume
-// up to the closing paren, if present, and whether the number parsed
-// ok. The bytes to consume will be 1 if no closing paren is present.
-func parseArgNumber(format string) (index int, wid int, ok bool) {
-	// There must be at least 3 bytes: [n].
-	if len(format) < 3 {
-		return 0, 1, false
-	}
-
-	// Find closing bracket.
-	for i := 1; i < len(format); i++ {
-		if format[i] == ']' {
-			width, ok, newi := parsenum(format, 1, i)
-			if !ok || newi != i {
-				return 0, i + 1, false
+			if enc, ok := arg.(fast.StringEncoder); ok {
+				enc.EncodeString(buf)
+			} else {
+				buf.WriteByte('$')
+				buf.WriteInt(argNum)
+				argNum++
 			}
-			return width - 1, i + 1, true // arg numbers are one-indexed and skip paren.
+
+			argIdx++
 		}
 	}
-	return 0, 1, false
-}
 
-// tooLarge reports whether the magnitude of the integer is
-// too large to be used as a formatting width or precision.
-func tooLarge(x int) bool {
-	const max int = 1e6
-	return x > max || x < -max
-}
-
-// parsenum converts ASCII to integer.  num is 0 (and isnum is false) if no number present.
-func parsenum(s string, start, end int) (num int, isnum bool, newi int) {
-	if start >= end {
-		return 0, false, end
-	}
-	for newi = start; newi < end && '0' <= s[newi] && s[newi] <= '9'; newi++ {
-		if tooLarge(num) {
-			return 0, false, end // Overflow; crazy long number most likely.
-		}
-		num = num*10 + int(s[newi]-'0')
-		isnum = true
-	}
-	return
+	buf.WriteString(format[cursor:])
 }
--- query_test.go
+++ query_test.go
@@ -2,58 +2,27 @@ package pg
 
 import (
 	"fmt"
-	"log"
 	"testing"
 
 	"github.com/webmafia/fast"
 )
 
-func Example_indexQuery() {
-	// buf := fast.NewStringBuffer(256)
-	q := indexQuery("SELECT * FROM %T WHERE foo = %d")
+func Example_encodeQuery() {
+	buf := fast.NewStringBuffer(256)
+	encodeQuery(buf, "SELECT * FROM %T WHERE foo = %d AND bar = %s AND baz = %s", []any{Table("trudeluttan"), 123, Table("mjau"), 456})
 
-	fmt.Printf("%#v", q)
+	fmt.Printf("%#v", buf.String())
 
 	// Output: Mjau
 }
 
-func Example_indexQuery_encode() {
+func Benchmark_encodeQuery(b *testing.B) {
 	buf := fast.NewStringBuffer(256)
-	q := indexQuery("SELECT * FROM %T WHERE foo = %d")
-
-	err := q.encodeQuery(buf, []any{Table("trudeluttan"), 123})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(buf.String())
-
-	// Output: Mjau
-}
-
-func Benchmark_indexQuery(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_ = indexQuery(`
-			SELECT *
-			FROM %T
-			WHERE foo = %d
-		`)
-	}
-}
-
-func Benchmark_indexQuery_encode(b *testing.B) {
-	buf := fast.NewStringBuffer(256)
-	q := indexQuery("SELECT * FROM %T WHERE foo = %d")
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		buf.Reset()
-		err := q.encodeQuery(buf, []any{Table("trudeluttan"), 123})
-
-		if err != nil {
-			b.Fatal(err)
-		}
+		encodeQuery(buf, "SELECT * FROM %T WHERE foo = %d", []any{Table("trudeluttan"), 123})
 	}
 }
