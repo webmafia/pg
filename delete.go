733062339f27b85d7730369d4b6aa68fa69c852a
Ivar Johansson <ivar@webbmaffian.se>
2024-03-13 18:06:21 +0100

    Delete

---
+++ delete.go
@@ -0,0 +1,23 @@
+package pg
+
+import "context"
+
+func (db *DB) Delete(ctx context.Context, table Identifier, cond QueryEncoder) (count int64, err error) {
+	inst := db.instPool.Acquire()
+	defer db.instPool.Release(inst)
+
+	inst.buf.WriteString("DELETE FROM ")
+	table.EncodeString(inst.buf)
+	inst.buf.WriteString(" WHERE ")
+	cond.EncodeQuery(inst.buf, &inst.args)
+
+	cmd, err := db.exec(ctx, inst.buf.String(), inst.args...)
+
+	if err != nil {
+		err = instError(err, inst)
+	} else {
+		count = cmd.RowsAffected()
+	}
+
+	return
+}
