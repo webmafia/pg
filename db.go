b3ce2b95f01bd9be0341f236dc396c5cbd72b7cf
Ivar Johansson <ivar@webbmaffian.se>
2024-10-23 10:53:04 +0200

    Breaking change: Simplified initialization

--- db.go
+++ db.go
@@ -1,6 +1,9 @@
 package pg
 
 import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/webmafia/fast"
 )
@@ -10,7 +13,42 @@ type DB struct {
 	valPool *fast.Pool[Values]
 }
 
-func NewDB(db *pgxpool.Pool) *DB {
+func New(ctx context.Context, connString string, alterConfig ...func(*pgxpool.Config)) (db *DB, err error) {
+	config, err := pgxpool.ParseConfig(connString)
+
+	if err != nil {
+		return
+	}
+
+	if len(alterConfig) > 0 && alterConfig[0] != nil {
+		alterConfig[0](config)
+	}
+
+	oldBeforeConnect := config.BeforeConnect
+	config.BeforeConnect = func(ctx context.Context, cc *pgx.ConnConfig) error {
+
+		if oldBeforeConnect != nil {
+			if err := oldBeforeConnect(ctx, cc); err != nil {
+				return err
+			}
+		}
+
+		cc.DefaultQueryExecMode = pgx.QueryExecModeCacheDescribe
+		cc.StatementCacheCapacity = 0
+
+		return nil
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+
+	if err != nil {
+		return
+	}
+
+	return newDB(pool), nil
+}
+
+func newDB(db *pgxpool.Pool) *DB {
 	return &DB{
 		db: db,
 		valPool: fast.NewPool(func(v *Values) {
--- example/example.go
+++ example/example.go
@@ -4,30 +4,16 @@ import (
 	"context"
 	"log"
 
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/webmafia/pg"
 )
 
 func example(ctx context.Context) (err error) {
-	config, err := pgxpool.ParseConfig("postgresql://postgres:postgres@localhost/postgres?sslmode=disable")
+	db, err := pg.New(ctx, "postgresql://postgres:postgres@localhost/postgres?sslmode=disable")
 
 	if err != nil {
 		return
 	}
 
-	config.BeforeConnect = func(ctx context.Context, cc *pgx.ConnConfig) error {
-		cc.DefaultQueryExecMode = pgx.QueryExecModeCacheDescribe
-		return nil
-	}
-
-	pool, err := pgxpool.NewWithConfig(ctx, config)
-
-	if err != nil {
-		return
-	}
-
-	db := pg.NewDB(pool)
 	defer db.Close()
 
 	if _, err = db.Exec(ctx, `INSERT INTO %T ("name") VALUES(%T)`, pg.Identifier("test"), "a"); err != nil {
